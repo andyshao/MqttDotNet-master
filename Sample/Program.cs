558b61c54c86fc0db9cb2b873394a00be7a8abce
gunnerlzh <253475443@qq.com>
2017-08-16 17:59:32 +0800

    8.16

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -9,15 +9,12 @@ namespace Sample
 {
 	class Program
 	{
-
-		//static ArrayList clientAs = ArrayList.Synchronized(new ArrayList());
-		//static ArrayList clientBs = ArrayList.Synchronized(new ArrayList());
-		static List<ClientWithState> clientAs = new List<ClientWithState>();
-		static List<ClientWithState> clientBs = new List<ClientWithState>();
-		//static List<Thread> threadAs = new List<Thread>();
-		//static List<Thread> threadBs = new List<Thread>();
-
-		static List<ManualResetEvent> manualEvents = new List<ManualResetEvent>();
+		static int clientNum = 100;
+		static int publishNum = 10;
+		static long publishArrivedCount = 0;
+		static int subFinishCount = 0;
+		static int pubFinishCount = 0;
+		static readonly object locker = new object();
 
 		static void Main(string[] args)
 		{
@@ -30,167 +27,142 @@ namespace Sample
 			Console.WriteLine("Starting MqttDotNet sample program.");
 			//Console.WriteLine("Press any key to stop\n");
 
-			for (int i = 1; i <= 20; i++)
-			{
-				var A = new ClientWithState("A", i, 2);
-				clientAs.Add(A);
-				//threadAs.Add(new Thread(A.PublishSometimes));
+			List<Program> clientAs = new List<Program>();
+			List<Program> clientBs = new List<Program>();
 
-				var B = new ClientWithState("B", i, 2);
-				clientBs.Add(B);
-				//threadBs.Add(new Thread(B.PublishSometimes));
-			}
-			foreach (ClientWithState clientA in clientAs)
-				clientA.ClientInit();
-			foreach (ClientWithState clientB in clientBs)
-				clientB.ClientInit();
-
-
-			foreach (ClientWithState clientA in clientAs)
-			{
-				ManualResetEvent mre = new ManualResetEvent(false);
-				manualEvents.Add(mre);
-				ThreadPool.QueueUserWorkItem(clientA.PublishSometimes, mre);
-			}
-			foreach (ClientWithState clientB in clientBs)
-			{
-				ManualResetEvent mre = new ManualResetEvent(false);
-				manualEvents.Add(mre);
-				ThreadPool.QueueUserWorkItem(clientB.PublishSometimes, mre);
-			}
-			//foreach (Thread threadA in threadAs)
-			//	threadA.Start();
-			//foreach (Thread threadB in threadBs)
-			//	threadB.Start();
-
-			//等待线程全部结束
-			WaitHandle.WaitAll(manualEvents.ToArray());
-			Thread.Sleep(5000);
-			PublishFailedSum();
-		}
-		
-		static void PublishFailedSum()
-		{
-			long publishArrivedSum = 0, publishSum = 0;
-			for (int i = 0; i < clientAs.Count; i++)
-			{
-				publishSum += clientAs[i].publishTimes;
-				publishArrivedSum += clientBs[i].publishArrivedTimes;
-			}
-			long publishFailedSum = publishSum - publishArrivedSum;
-			Console.WriteLine("publishFailedSum of clientAs is {0}\n{1}\n{2}", publishFailedSum, publishSum, publishArrivedSum);
-		}
-
-
-		class ClientWithState
-		{
-
-			private string clientGroup;
-			private int clientNum;
-			private int _publishTimes;
-			private int _publishArrivedTimes;
-
-			public int publishTimes
-			{
-				get	{ return _publishTimes; }
-				set	{ _publishTimes = value; }
-			}
-			public int publishArrivedTimes
-			{
-				get { return _publishArrivedTimes; }
-				//set { _publishArrivedTimes = 0; }
-			}
-
-			public ClientWithState(string group, int num, int t)
-			{
-				clientGroup = group;
-				clientNum = num;
-				publishTimes = t;
-			}
-
-			String clientID = Guid.NewGuid().ToString();//.Substring(0,23);
 			String connString = "tcp://127.0.0.1:11883";
-			IMqtt _client;
 
-			public void ClientInit()
+			for (int i = 1; i <= clientNum; i++)
 			{
-				// Instantiate client using MqttClientFactory
-				_client = MqttClientFactory.CreateClient(connString, clientID);
-
-				// Setup some useful client delegate callbacks
-				_client.Connected += new ConnectionDelegate(client_Connected);
-				_client.ConnectionLost += new ConnectionDelegate(_client_ConnectionLost);
-				_client.PublishArrived += new PublishArrivedDelegate(client_PublishArrived);
-				Start();
+				String clientID = Guid.NewGuid().ToString().Substring(0,23);
+				var A = new Program(connString, clientID, "A", i);
+				clientAs.Add(A);
+				new Thread(A.Start).Start();
+			}
+			for (int i = 1; i <= clientNum; i++)
+			{
+				String clientID = Guid.NewGuid().ToString().Substring(0,23);
+				var B = new Program(connString, clientID, "B", i);
+				clientAs.Add(B);
+				new Thread(B.Start).Start();;
 			}
 
-			public void PublishSometimes(object obj)
+			while (subFinishCount != clientNum * 2)
+				;
+			Console.WriteLine("****************Client Subscribe All Finished!****************\n");
+			Thread.Sleep(3000);
+
+			foreach (Program clientA in clientAs)
 			{
-				lock (this)
+				for (int i = 1; i <= publishNum; i++)
 				{
-					for (int i = 1; i <= _publishTimes; i++)
-						this.PublishSomething();
+					new Thread(clientA.PublishSomething).Start();
 				}
-				ManualResetEvent mre = (ManualResetEvent)obj;
-				mre.Set();
-
 			}
-
-
-			void Start()
+			foreach (Program clientB in clientBs)
 			{
-				// Connect to broker in 'CleanStart' mode
-				Console.WriteLine("Client connecting\n");
-				_client.Connect(true);
-			}
-
-			void Stop()
-			{
-				if (_client.IsConnected)
+				for (int i = 1; i <= publishNum; i++)
 				{
-					Console.WriteLine("Client disconnecting\n");
-					_client.Disconnect();
-					Console.WriteLine("Client disconnected\n");
+					new Thread(clientB.PublishSomething).Start();
 				}
 			}
 
-			void client_Connected(object sender, EventArgs e)
+			long publishNumSum = clientNum * 2 * publishNum;
+			while (pubFinishCount != publishNumSum)
+				;
+			Thread.Sleep(1000);
+			Console.WriteLine("****************Client Publish All Finished!****************\n");
+			//double lossRate = (publishNumSum - publishArrivedCount) / publishNumSum;
+			//Console.WriteLine("The Loss Rate is {0}%", (lossRate * 100).ToString("0.00"));
+			long publishLostCount = publishNumSum - publishArrivedCount;
+			Console.WriteLine("The Lost Count Is {0}", publishLostCount.ToString());
+		}
+
+		IMqtt _client;
+		string clientGroup { get; set; }
+		int clientCount { get; set; }
+
+		Program(string connectionString, string clientId, string group, int count)
+		{
+			clientGroup = group;
+			clientCount = count;
+			// Instantiate client using MqttClientFactory
+			_client = MqttClientFactory.CreateClient(connectionString, clientId);
+
+			// Setup some useful client delegate callbacks
+			_client.Connected += new ConnectionDelegate(client_Connected);
+			_client.ConnectionLost += new ConnectionDelegate(_client_ConnectionLost);
+			_client.PublishArrived += new PublishArrivedDelegate(client_PublishArrived);
+		}
+
+		void Start()
+		{
+			// Connect to broker in 'CleanStart' mode
+			Console.WriteLine("Client {0}{1} connecting", clientGroup, clientCount.ToString());
+			_client.Connect(true);
+		}
+
+		void Stop()
+		{
+			if (_client.IsConnected)
 			{
-				Console.WriteLine("Client connected\n");
+				Console.WriteLine("Client disconnecting");
+				_client.Disconnect();
+				Console.WriteLine("Client disconnected");
+			}
+		}
+
+		void client_Connected(object sender, EventArgs e)
+		{
+			lock (locker)
+			{
+				Console.WriteLine("Client {0}{1} connected", clientGroup, clientCount.ToString());
 				RegisterOurSubscriptions();
 				//PublishSomething();
 			}
-			void _client_ConnectionLost(object sender, EventArgs e)
+		}
+		void _client_ConnectionLost(object sender, EventArgs e)
+		{
+			lock (locker)
 			{
-				Console.WriteLine("Client connection lost\n");
-				//this.Start();
+				Console.WriteLine("Client connection lost");
+				Start();
+				subFinishCount--;
 			}
+		}
 
-			void RegisterOurSubscriptions()
+		void RegisterOurSubscriptions()
+		{
+			lock (locker)
 			{
-				Console.WriteLine("Subscribing to mqttdotnet/" + clientGroup + clientNum.ToString() + "\n");
-				_client.Subscribe("mqttdotnet/" + clientGroup + clientNum.ToString(), QoS.BestEfforts);
-				Console.WriteLine("Subscribed");
+				Console.WriteLine("Subscribing to mqttdotnet/" + clientGroup + clientCount.ToString());
+				_client.Subscribe("mqttdotnet/" + clientGroup + clientCount.ToString(), QoS.BestEfforts);
+				subFinishCount++;
 			}
+		}
 
-			public void PublishSomething()
+		void PublishSomething()
+		{
+			lock (locker)
 			{
-				string clientGroup_= (clientGroup == "A")?"B":"A";
-				Console.WriteLine("Publishing on mqttdotnet/" + clientGroup_ + clientNum.ToString() + "\n");
-				_client.Publish("mqttdotnet/" + clientGroup_ + clientNum.ToString(), "Hello MQTT World", QoS.OnceAndOnceOnly, false);
+				string _clientGroup = (clientGroup == "A") ? "B" : "A";
+				Console.WriteLine("Publishing on mqttdotnet/" + _clientGroup + clientCount.ToString());
+				_client.Publish("mqttdotnet/" + _clientGroup + clientCount.ToString(), "Hello MQTT World", QoS.OnceAndOnceOnly, false);
+				pubFinishCount++;
 			}
+		}
 
-			bool client_PublishArrived(object sender, PublishArrivedArgs e)
+		bool client_PublishArrived(object sender, PublishArrivedArgs e)
+		{
+			lock (locker)
 			{
-				Console.WriteLine("Received Message");
-				Console.WriteLine("Topic: " + e.Topic);
-				Console.WriteLine("Payload: " + e.Payload);
-				Console.WriteLine();
-				_publishArrivedTimes++;
+				//Console.WriteLine("Received Message");
+				//Console.WriteLine("Topic: " + e.Topic);
+				//Console.WriteLine("Payload: " + e.Payload);
+				//Console.WriteLine();
+				publishArrivedCount++;
 				return true;
 			}
-
 		}
 	}
-
 }
